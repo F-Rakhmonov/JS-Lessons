f4831dff658a1e56cdc7c4a21d98e8a1509e460a
F-Rakhmonov <f-rakhmonov@list.ru>
2024-02-13 20:55:50 +0500

    fuctions

--- index.js
+++ index.js
@@ -17,9 +17,34 @@
 
 // default parametros
 
-let name='Asildin'
-function decl(name1,name2='Nicol'){
-    console.log(`Hello ${name1} and ${name2}`);
-}
-decl("Adam",name);
+// let name='Asildin'
+// function decl(name1,name2='Nicol'){
+//     console.log(`Hello ${name1} and ${name2}`);
+// }
+// decl("Adam",name);
 
+
+
+// function with return ============
+// 1)  arrow function with return 
+
+// const myFunc=()=>{
+//     return 10+2
+// }
+
+// console.log(myFunc()); // 12
+
+// 2) function expression with return
+
+// const myFuncExp=function(){
+//     return 'Hi'
+// }
+// console.log(myFuncExp()); //Hi
+
+// 3) function decloration
+
+// function myFuncDecl(){
+//     return 'Hi'
+// }
+
+// console.log(myFuncDecl()); //Hi
