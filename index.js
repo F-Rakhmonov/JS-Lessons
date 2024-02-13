e9ebc409144084cb2fddb56a16ab969dcc7fb61c
F-Rakhmonov <f-rakhmonov@list.ru>
2024-02-13 20:32:25 +0500

    fuctions

---
+++ index.js
@@ -0,0 +1,15 @@
+// decloration
+function decloration(){
+console.log('Hello decloration');
+}
+decloration()
+
+// expression
+const telegram=function(){
+    console.log('Hello expression');
+}
+telegram()
+
+// arrow
+const myFunction=()=>console.log('Hello arrow');
+myFunction()
