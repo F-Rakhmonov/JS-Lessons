cf9c9915f72e42c97e65384fdda9413770016d18
F-Rakhmonov <f-rakhmonov@list.ru>
2024-02-13 20:41:16 +0500

    function default params and with varibles

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // decloration
-function decloration(){
-console.log('Hello decloration');
-}
-decloration()
+// function decloration(){
+// console.log('Hello decloration');
+// }
+// decloration()
 
 // expression
-const telegram=function(){
-    console.log('Hello expression');
-}
-telegram()
+// const telegram=function(){
+//     console.log('Hello expression');
+// }
+// telegram()
 
 // arrow
-const myFunction=()=>console.log('Hello arrow');
-myFunction()
+// const myFunction=()=>console.log('Hello arrow');
+// myFunction()
+
+
+// default parametros
+
+let name='Asildin'
+function decl(name1,name2='Nicol'){
+    console.log(`Hello ${name1} and ${name2}`);
+}
+decl("Adam",name);
+
